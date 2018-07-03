4eb2627b0b2812a794e6020f37eab2dda8456a20
Rich Gwozdz <rich.gwozdz@gmail.com>
2018-07-03 13:04:51 -0700

    Cleanup.

--- update.js
+++ update.js
@@ -1,22 +1,20 @@
-const fs = require('fs')
 const del = require('del')
+const Git = require('nodegit')
 const { diffAndWrite } = require('./lib')
 const oldTag = 'v1.0.3'
-const latestTag = 'v1.0.4'
-const Git = require("nodegit");
-let repo
+const latestTag = 'v1.0.4'  // eslint-disable-line
 
-(async function () {
+(async function () { // eslint-disable-line
   try {
     // Checkout old release
-    const repoOld = await Git.Clone("https://github.com/Esri/proj-codes", oldTag)
+    const repoOld = await Git.Clone('https://github.com/Esri/proj-codes', oldTag)
     const branchOld = await repoOld.getBranch(`refs/tags/${oldTag}`)
-    await repoOld.checkoutRef(branchOld);
+    await repoOld.checkoutRef(branchOld)
 
     // Checkout latest release
-    const repoLatest = await Git.Clone("https://github.com/Esri/proj-codes", latestTag)
+    const repoLatest = await Git.Clone('https://github.com/Esri/proj-codes', latestTag)
     const branchLatest = await repoLatest.getBranch(`refs/tags/${latestTag}`)
-    await repoLatest.checkoutRef(branchLatest);
+    await repoLatest.checkoutRef(branchLatest)
 
     // Diff the releases and write to JSON files
     diffAndWrite(`./${oldTag}/`, `./${latestTag}/`)
@@ -27,4 +25,4 @@ let repo
     await del(`./${oldTag}`)
     await del(`./${latestTag}`)
   }
-}) ()
+})()
